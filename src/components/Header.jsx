bd5b2d32d3596380a0958cbb387d7e53c0c7cd26
Ashmin <ashmin2802@gmail.com>
2024-08-09 12:41:38 +0530

    Finish name and icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,10 @@ function Header() {
     <div>
       <div className="flex justify-between items-start ">
         <span><i class="fa-solid fa-bars-staggered fa-xl"></i></span>
+        <div className="flex items-center">
+        <span><i class="fa-regular fa-bell fa-xl me-3"></i></span>
         <span><Link to={'/'}><i class="fa-solid fa-circle-user text-4xl text-black"></i></Link></span>
+        </div>
       </div>
     </div>
   )
